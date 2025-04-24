71f50ad2dc5265f985ee37d113014459e4dc3ef5
K B POOVANNA <89855214+kbpoovanna-007@users.noreply.github.com>
2025-04-24 23:14:01 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
-// Auth functions
 export const signup = async (userData) => {
   try {
     const response = await fetch(`${API_URL}/signup`, {
